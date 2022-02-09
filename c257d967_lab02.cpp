fe5ff50a2132672f4073815f5929f181ca51c794
ebeason <98134938+ebeason@users.noreply.github.com>
2022-02-08 19:42:19 -0600

    Create c257d967_lab02.cpp
    Created for loop that starts at 13 and adds multiples until reaching 260.

---
+++ c257d967_lab02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+  /*
+  * Ethan Beason
+  * C257D967
+  * Lab 2
+  */
+
+int main()
+{
+    for (int i = 13; i <= 260; i = i + 13)
+        std::cout << i << "\n";
+    return 0;
+}
